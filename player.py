342ecf8ace46cd762c30443d7014edb134998715
Yakrats <greenyoshi429@gmail.com>
2024-09-20 00:05:25 -0700

    5th commit: coded forward/backward movement

--- player.py
+++ player.py
@@ -22,6 +22,12 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        # Update moving foward(w) and backward(s)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        # Update rotating left(a) and right(d)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -29,3 +35,7 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
